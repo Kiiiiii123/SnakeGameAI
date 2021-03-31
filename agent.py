f12a68aaa6ed90b1ec8a557f2952f8fcc2050da7
zhangchao <zf1721418@buaa.edu.cn>
2021-03-31 23:15:49 +0800

    update the agent.py

--- agent.py
+++ agent.py
@@ -15,22 +15,83 @@ class Agent:
         self.epsilon = 0  # to control the randomness
         self.gamma = 0  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)  # pop left
-        # TODO: model, trainer
+        self.model = None
+        self.trainer = None
 
     def get_state(self, env):
-        pass
+        head = env.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y + 20)
+        point_d = Point(head.x, head.y - 20)
+
+        dir_l = env.snake_direction == Direction.LEFT
+        dir_r = env.snake_direction == Direction.RIGHT
+        dir_u = env.snake_direction == Direction.UP
+        dir_d = env.snake_direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_l and env.is_collision(point_l))
+            or (dir_r and env.is_collision(point_r))
+            or (dir_u and env.is_collision(point_u))
+            or (dir_d and env.is_collision(point_d)),
+            # Danger right
+            (dir_l and env.is_collision(point_u))
+            or (dir_r and env.is_collision(point_d))
+            or (dir_u and env.is_collision(point_r))
+            or (dir_d and env.is_collision(point_l)),
+            # Danger left
+            (dir_l and env.is_collision(point_d))
+            or (dir_r and env.is_collision(point_u))
+            or (dir_u and env.is_collision(point_l))
+            or (dir_d and env.is_collision(point_r)),
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            # Food location
+            env.food.x < env.head_position.x,  # food left
+            env.food.x > env.head_position.x,  # food right
+            env.food.y < env.head_position.y,  # food down
+            env.food.y > env.head_position.y,  # food up
+        ]
+
+        return np.array(state, dtype=int)
 
     def store_data(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append(
+            (state, action, reward, next_state, done)
+        )  # popleft if MAX_MEMORY is reached
 
     def train_long_memory(self):
-        pass
+        # grab one thousand samples from the memory
+        if len(self.memory) > BATCH_SIZE:
+            batch_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            batch_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*batch_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        # random moves: trade-off between exploration and exploitation
+        self.epsilon = 80 - self.num_games
+        move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move_idx = random.randint(0, 2)
+            move[move_idx] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model.predict(state0)
+            move_idx = torch.argmax(prediction).item()
+            move[move_idx] = 1
+
+        return move
 
 
 def train():
@@ -70,7 +131,7 @@ def train():
                 record_score = score
                 # TODO agent.model.save()
 
-            print("Game", agent.num_games, "Score", score, "REcord", record_score)
+            print("Game", agent.num_games, "Score", score, "Record", record_score)
 
             # TODO: plot
 
